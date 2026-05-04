ecfd1729fa29ba6a261d2f0b18cdf6aede01da30
vash_huang <Vash_Huang@asus.com>
2026-05-04 17:00:13 +0800

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        version = os.getenv("APP_VERSION", "unknown")
+        body = f"version: {version}\n".encode()
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(body)
+
+    def log_message(self, *args):
+        pass  # suppress default access log
+
+
+HTTPServer(("", 8080), Handler).serve_forever()
